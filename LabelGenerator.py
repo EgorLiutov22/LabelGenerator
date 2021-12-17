741ba51a1b252c1d1d6c8bfbdc5b4faf64beafcf
EgorLiutov22 <93601126+EgorLiutov22@users.noreply.github.com>
2021-12-17 15:33:39 +0500

    Update LabelGenerator.py
    some bug fixes

--- LabelGenerator.py
+++ LabelGenerator.py
@@ -4,8 +4,8 @@ import barcode
 from barcode.writer import ImageWriter
 df = pd.read_excel('./barcodes.xlsx', index_col = 'indexes')
 lenght = df.shape[0]
-H = 456 #set label size ( this size is for 58 mm * 40 mm labels)
-W = 314
+W = 456 #set label size ( this size is for 58 mm * 40 mm labels)
+H = 314
 i = 1
 while i in range(0,lenght+1):
     price = df.loc[i]['price']
@@ -16,7 +16,7 @@ while i in range(0,lenght+1):
     bc = barcode.get('code128', str(barcodenum), writer=ImageWriter())
     bcadr = bc.save('bc' + nomenclature)
     bcimg = Image.open(bcadr)
-    new_img = Image.new('1', (H, W), 'white')
+    new_img = Image.new('1', (W, H), 'white')
     font = ImageFont.truetype(r'./AlkesLight.ttf', size = 25) #set fonts and sizes
     font2 = ImageFont.truetype(r'./AlkesLight.ttf', size = 35)
     font3 = ImageFont.truetype(r'./AlkesLight.ttf', size = 35)
@@ -28,11 +28,11 @@ while i in range(0,lenght+1):
     wp, hp = pencil.textsize(price_print,  font = font3)
     wb1, hb1 = fontbar.getsize(price_print)
     wb2, hb2 = fontbarlow.getsize(price_print)
-    resizedbc = bcimg.resize((int(W), int(H/3.5)), Image.ANTIALIAS)
-    pencil.text(((H-wc)/2,10), companyname,  font = font, fill = 'black', encoding = 'UTF-8') #draw texts
-    pencil.text(((H-wn)/2,70), nomenclature,  font = font2, fill = 'black', encoding = 'UTF-8')
-    pencil.text(((H-wp)/2,140),  price_print,  font = font3, fill = 'black', encoding = 'UTF-8')
-    new_img.paste(resizedbc, (int(W/3.5),180)) #paste barcode into label
+    resizedbc = bcimg.resize((int(W), int(H/2.5)), Image.ANTIALIAS)
+    pencil.text(((W-wc)/2,10), companyname,  font = font, fill = 'black', encoding = 'UTF-8') #draw texts
+    pencil.text(((W-wn)/2,70), nomenclature,  font = font2, fill = 'black', encoding = 'UTF-8')
+    pencil.text(((W-wp)/2,140),  price_print,  font = font3, fill = 'black', encoding = 'UTF-8')
+    new_img.paste(resizedbc, (int(W/100),180)) #paste barcode into label
     new_img.show() #show result
 
     new_img.save(nomenclature + '.png') #save result
